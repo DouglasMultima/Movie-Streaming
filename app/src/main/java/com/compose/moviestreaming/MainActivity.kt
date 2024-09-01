5802f2d6719e5b3f90987b6cfa904cc0dec802c4
Douglas <douglas.multima@hotmail.com>
2024-08-31 23:31:15 -0400

    Layout Splash

---
+++ app/src/main/java/com/compose/moviestreaming/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.compose.moviestreaming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.compose.moviestreaming.presenter.splash.SplashScreen
+import com.compose.moviestreaming.ui.theme.MovieStreamingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MovieStreamingTheme {
+
+                SplashScreen()
+
+
+            }
+        }
+    }
+}
+
